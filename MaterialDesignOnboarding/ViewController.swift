a9bebd28b91de38fb6a7e98c564b7fbb723f37aa
Fredrick Ohen <fredrickohen@jasmines-macbook-pro.local>
2017-01-20 13:51:01 -0600

    Fixed spacing issues

--- MaterialDesignOnboarding/ViewController.swift
+++ MaterialDesignOnboarding/ViewController.swift
@@ -10,44 +10,35 @@ import UIKit
 import paper_onboarding
 
 class ViewController: UIViewController, PaperOnboardingDataSource {
-
-    @IBOutlet weak var onboardingView: OnboardingView!
-   
+  
+  @IBOutlet weak var onboardingView: OnboardingView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    onboardingView.dataSource = self
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        onboardingView.dataSource = self
-        
     
-    }
-    // Required methods from Onboarding Data Source
-    func onboardingItemsCount() -> Int {
-        return 3
-    }
-
-    func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo {
-        let backgroundColorOne = UIColor(red: 217/255, green: 72/255, blue: 89/255, alpha: 1)
-        let backgroundColorTwo = UIColor(red: 106/255, green: 106/255, blue: 211/255, alpha: 1)
-        let backgroundColorThree = UIColor(red: 168/255, green: 200/255, blue: 78/255, alpha: 1)
-        
-        let titleFont = UIFont(name: "AvenirNext-Bold", size: 24)!
-        let descriptionFont = UIFont(name: "AvenirNext-Regular", size: 18)!
-        
-        return [("rocket", "Space City", "Why do we still use outdated onboard design? With Google's new Material Design", "", backgroundColorOne, UIColor.white, UIColor.white, titleFont, descriptionFont),
-    // Second Screen
-                ("brush", "Powerful Rocket", "Your app will look modern and professional", "", backgroundColorTwo, UIColor.white, UIColor.white, titleFont, descriptionFont),
-    // Third Screen
-                ("notification", "Innovative design", "Visually appealing", "", backgroundColorThree, UIColor.white, UIColor.white, titleFont, descriptionFont)
-                ][index]
-
-    }
+  }
+// Required methods from Onboarding Data Source
+  func onboardingItemsCount() -> Int {
+    return 3
+  }
+  
+  func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo {
+    let backgroundColorOne = UIColor(red: 217/255, green: 72/255, blue: 89/255, alpha: 1)
+    let backgroundColorTwo = UIColor(red: 106/255, green: 106/255, blue: 211/255, alpha: 1)
+    let backgroundColorThree = UIColor(red: 168/255, green: 200/255, blue: 78/255, alpha: 1)
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    let titleFont = UIFont(name: "AvenirNext-Bold", size: 24)!
+    let descriptionFont = UIFont(name: "AvenirNext-Regular", size: 18)!
+    
+    return [("rocket", "Space City", "Why do we still use outdated onboard design? With Google's new Material Design", "", backgroundColorOne, UIColor.white, UIColor.white, titleFont, descriptionFont),
+// Second Screen
+      ("brush", "Powerful Rocket", "Your app will look modern and professional", "", backgroundColorTwo, UIColor.white, UIColor.white, titleFont, descriptionFont),
+// Third Screen
+      ("notification", "Innovative design", "Visually appealing", "", backgroundColorThree, UIColor.white, UIColor.white, titleFont, descriptionFont)
+      ][index]
+    
+  }
 }
 
